e7c686da47c92c1e643343bcacba19ef9c7515f9
Andrew Christiansen <chrian10@isu.edu>
2020-02-25 11:04:39 -0700

    Renamed example files and fixed import issues

--- Examples/concept_query_example.py
+++ Examples/concept_query_example.py
@@ -4,7 +4,7 @@ import importlib.util
 # Loads module from parent directory until we restructure the files
 # Snippet from:
 # https://stackoverflow.com/questions/67631/how-to-import-a-module-given-the-full-path
-spec = importlib.util.spec_from_file_location("ConceptInquirer", os.getcwd() + "\ConceptInquirer.py")
+spec = importlib.util.spec_from_file_location("ConceptInquirer", os.getcwd() + "\concept_query.py")
 module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
 
@@ -13,5 +13,5 @@ ci = module.ConceptInquirer('dog')
 
 relationships = ci.get_is_a_relationships()
 
-for r in relationships:
-    print(r)
+for relation in relationships.items():
+    print(relation)
