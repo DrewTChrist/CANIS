aa3d60321c9259b08f4d95c6f73bbf3088d0b97e
unknown <sewellandres@gmail.com>
2020-02-28 17:29:24 -0700

    Remove pointless argument from build_graph

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from processor import ImageProcessor
 
 # New test images can be placed in the /img folder, then manually change
 # image_name accordingly
-image_name = 'sky2.jpg'
+image_name = 'sky1.jpg'
 
 # Open a new image, store it's original form
 ip = ImageProcessor(os.path.join(os.path.curdir, "./img/" + image_name))
@@ -15,7 +15,7 @@ original = ip.img
 # Process the image and build a networkx graph of each star
 ip.process()
 ip.extract_vertices()
-ip.build_graph(type="subset")
+ip.build_graph()
 
 # Generate a new pattern specifying minimum fitness (0 by default until a
 # proper implementation is in place)
